ff10c26eecd4b1fb5d5634650c89d9da01e87dfa
Kaila Santiago <122849379+santiikai@users.noreply.github.com>
2023-02-06 16:28:15 -0500

    updated parallax effect

--- script.js
+++
@@ -1,8 +0,0 @@
-const mobileView = document.getElementById("mobileView");
-mobileView.addEeventListener("click", function(e) {
-    if(document.getElementsByTagName('meta')['viewport'].content=='width= 1440px;'){
-        document.getElementsByTagName('meta')['viewport'].content='width= 400px;';
-       }else{
-        document.getElementsByTagName('meta')['viewport'].content='width= 1440px;';
-       }
-});
